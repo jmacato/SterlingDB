10892860cf5bc953f032490c4c2e2b44a588206a
Jumar Macato <hikari.netto23@gmail.com>
2020-05-26 10:46:41 +0800

    add cleanup method to test base

--- SterlingDB.Tests/TestBase.cs
+++ SterlingDB.Tests/TestBase.cs
@@ -8,11 +8,35 @@ using SterlingDB.Core;
 
 namespace SterlingDB.Test
 {
-    public abstract class TestBase
+    public abstract class TestBase : IDisposable
     {
+        private bool disposedValue;
+
         protected virtual ISterlingDriver GetDriver()
         {
             return new MemoryDriver();
+        }
+
+        public abstract void Cleanup();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Cleanup();
+                }
+
+                disposedValue = true;
+            }
+        }
+ 
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
